24c71dfebb01c31d51506db0688f4cd10bae1931
nk <nktuhin12@gmail.com>
2019-12-14 01:00:55 +0600

    Django Rest_Framework with JWT Authentication

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('jsonapi.urls')),
+
+    # rest_framework urls path setup
+    path('api-auth', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view())
+]
---
+++ jsonapi/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register('paradigms', views.ParadigmView)
+router.register('languages', views.LanguageView)
+router.register('programmers', views.ProgrammerView)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
